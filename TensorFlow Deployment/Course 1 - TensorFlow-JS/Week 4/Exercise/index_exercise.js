65eca8ea62dc57b815e4bd4ce6fe174dbc76479d
--local <cj.bayron@congrego.co>
2020-12-19 17:58:23 +0800

    updates

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -25,20 +25,20 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+        tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+        tf.layers.dense({ units: 256, activation: 'relu'}),
+        tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -76,6 +76,10 @@ function handleButton(elem){
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE
+    case "4":
+      lizardSamples++;
+      document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+      break;
 		
             
 	}
@@ -113,6 +117,9 @@ async function predict() {
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE 
+    case 4:
+      predictionText = "I see Lizard";
+      break;
 	
             
 	}
